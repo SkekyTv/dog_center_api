3208e762df87e2e0a6acb9eed06aad3d8a538ce5
SkekyTv <skeky.stream@gmail.com>
2025-05-05 08:30:40 +0200

    chore: Use dogs construtor and add unit test

--- src/entities/dogs.rs
+++ src/entities/dogs.rs
@@ -10,6 +10,36 @@ pub struct Dogs {
     pub name: String,
     pub birthdate: Option<DateTime<Utc>>,
     pub races: Vec<String>,
-    pub img_url: Option<String>,
+    pub img_url: Option<String>, // unimplemented!()
     pub sex: Sex,
+}
+
+impl Dogs {
+    pub fn new(
+        name: String,
+        sex: Sex,
+        birthdate: Option<DateTime<Utc>>,
+        races: Vec<String>,
+    ) -> Self {
+        Dogs {
+            id: Uuid::new_v4(),
+            name,
+            birthdate,
+            races,
+            sex,
+            img_url: None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dog_constructor_minimal_params() {
+        let dog = Dogs::new("pupuce".to_string(), Sex::F, None, [].to_vec());
+        assert_eq!(dog.name, "pupuce");
+        assert_eq!(dog.sex, Sex::F);
+    }
 }
--- src/interfaces/http/dogs/handlers.rs
+++ src/interfaces/http/dogs/handlers.rs
@@ -4,22 +4,12 @@ use axum::{
     http::StatusCode,
 };
 use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use uuid::Uuid;
 
 use crate::{app_state::AppState, entities::dogs::Dogs, shared::types::sex::Sex};
 
 use validator::Validate;
-
-#[derive(Serialize)]
-pub struct DogsResponse {
-    pub id: Uuid,
-    pub name: String,
-    pub birthdate: Option<DateTime<Utc>>,
-    pub races: Vec<String>,
-    pub img_url: Option<String>,
-    pub sex: Sex,
-}
 
 #[derive(Debug, Deserialize, Validate)]
 pub struct CreateDogRequest {
@@ -36,7 +26,7 @@ pub struct CreateDogRequest {
 pub async fn get_dog_handler(
     Path(id): Path<Uuid>,
     State(state): State<AppState>,
-) -> Result<Json<DogsResponse>, (StatusCode, String)> {
+) -> Result<Json<Dogs>, (StatusCode, String)> {
     let dogs_service = &state.dogs_service;
 
     let dog = dogs_service
@@ -45,7 +35,7 @@ pub async fn get_dog_handler(
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
     match dog {
-        Some(dog) => Ok(Json(DogsResponse {
+        Some(dog) => Ok(Json(Dogs {
             id: dog.id,
             name: dog.name,
             birthdate: dog.birthdate,
@@ -61,14 +51,7 @@ pub async fn create_dog_handler(
     State(state): State<AppState>,
     Json(payload): Json<CreateDogRequest>,
 ) -> Result<(StatusCode, Json<Dogs>), (StatusCode, String)> {
-    let dog = Dogs {
-        id: Uuid::new_v4(),
-        name: payload.name,
-        birthdate: payload.birthdate,
-        races: payload.races,
-        img_url: None,
-        sex: payload.sex,
-    };
+    let dog = Dogs::new(payload.name, payload.sex, payload.birthdate, payload.races);
 
     let repo = &state.dogs_service;
 
