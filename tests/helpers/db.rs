000dddf355ab617333d4b1a396b2ee87d0748d4e
SkekyTv <skeky.stream@gmail.com>
2025-05-14 08:20:59 +0200

    test: Work in local

--- tests/helpers/db.rs
+++ tests/helpers/db.rs
@@ -1,64 +1,53 @@
-use dog_center_api::db;
+use sqlx::pool::PoolOptions;
 use sqlx::{Pool, Postgres};
-use testcontainers::{ImageExt, runners::AsyncRunner};
-use testcontainers_modules::postgres;
-use tracing::{error, info};
+use std::time::Duration;
+use testcontainers::core::ContainerPort;
+use testcontainers::runners::AsyncRunner;
+use testcontainers::{GenericImage, ImageExt};
 
 pub async fn setup_test_postgres() -> Pool<Postgres> {
+    // Configuration de l'image PostgreSQL
     let db_user = std::env::var("POSTGRES_USER").expect("POSTGRES_USER must be set");
     let db_pdw = std::env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD must be set");
     let db_name = std::env::var("POSTGRES_DATABASE").expect("POSTGRES_DATABASE must be set");
-
-    let os = std::env::var("OS").expect("OS should be set to configure docker");
-    let network = match os.as_str() {
-        "mac" => "bridge",
-        "windows" => "bridge",
-        "linux" => "host",
-        _ => panic!("Unsuportedd OS: {}", os),
-    };
-
-    let _container = match postgres::Postgres::default()
-        .with_network(network)
-        .with_env_var("POSTGRES_USER", db_user)
-        .with_env_var("POSTGRES_PASSWORD", db_pdw)
-        .with_env_var("POSTGRES_DB", db_name)
+    let container_port = ContainerPort::from(5432);
+    let container = GenericImage::new("postgres", "latest")
+        .with_exposed_port(container_port)
+        .with_env_var("POSTGRES_USER", &db_user)
+        .with_env_var("POSTGRES_PASSWORD", &db_pdw)
+        .with_env_var("POSTGRES_DB", &db_name)
+        .with_env_var("POSTGRES_HOST_AUTH_METHOD", "trust")
         .start()
         .await
-    {
-        Ok(container) => {
-            info!("docker start");
-            container
-        }
-        Err(e) => {
-            error!("error docker {}", e);
-            panic!("Failed to create docker container")
-        }
-    };
+        .unwrap();
 
-    // match container.get_host_port_ipv4(5432).await {
-    //     Ok(port) => {
-    //         info!("PostgreSQL is exposed on port {}", port);
-    //         port
-    //     }
-    //     Err(_) => {
-    //         error!("Failed to get exposed port for PostgreSQL.");
-    //         panic!("PostgreSQL port mapping failed");
-    //     }
-    // };
-    let connection_string = db::generate_db_url_from_env();
+    // Récupérer le port exposé
+    let host = container.get_host().await.unwrap();
+    let host_port = container.get_host_port_ipv4(5432).await.unwrap();
 
-    // Créer un pool Postgres SQLx
-    let pool = sqlx::PgPool::connect(&connection_string)
+    // Construire la chaîne de connexion
+    let connection_string = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        db_user, db_pdw, host, host_port, db_name
+    );
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
+    // Créer un pool SQLx
+    let pool = PoolOptions::<Postgres>::new()
+        .max_connections(30) // Définir le nombre maximum de connexions
+        .min_connections(5)
+        .connect(&connection_string)
         .await
         .expect("Failed to connect to PostgreSQL");
 
-    info!("Test pool started.");
-
-    // Exécuter les migrations si nécessaire
+    // Appliquer les migrations (si nécessaire)
     sqlx::migrate!()
         .run(&pool)
         .await
         .expect("Failed to run migrations");
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
 
     pool
 }
